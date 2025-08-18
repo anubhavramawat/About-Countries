3547d3a1326064dcc06ff3a356a5438dcaeeeff5
Anubhav Ramawat <ramawatanubhav2002@gmail.com>
2025-08-18 23:06:45 +0530

    udated api url

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -7,7 +7,7 @@ export const CountriesList = ({ query }) => {
     const [countriesData, setCountriesData] = useState([])
 
     useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all')
+        fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital')
           .then((res) => res.json())
           .then((data) => {
             setCountriesData(data)
